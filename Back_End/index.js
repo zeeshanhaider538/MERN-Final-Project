9e9b1f9cba2fadc411d7a947df9e5632bad63f08
Zeeshan Haider <zeeshanhaider538@gmail.com>
2023-02-10 10:26:24 +0500

    set up backend

--- Back_End/index.js
+++ Back_End/index.js
@@ -1,5 +1,13 @@
+// Express
 const express = require("express");
 const app = express();
 const port = 4000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-app.get("/", (req, res) => res.send("hello"))
+app.get("/", (req, res) => res.send("hellow oay jani"))
+
+// Mongoose
+
+const Mongoose=require("mongoose");
+
+// connecting to Data Base
+Mongoose.connect("mongodb://127.0.0.1:27017:ExerciseTracker",{useNewUrlParser:true})
